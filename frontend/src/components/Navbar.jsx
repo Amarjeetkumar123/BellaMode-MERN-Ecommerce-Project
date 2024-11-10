df8b2126b8678682037aed5694d0a28c5e7f70c8
Amarjeet Kumar <amarjeetkumar802212@gmail.com>
2024-11-10 22:26:22 +0530

    completed login or register page ui

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     return (
         <>
             <header className='fixed-nav-bar w-nav'>
-                <nav className='max-w-screen-2xl mx-auto px-4 flex justify-between items-center'>
+                <nav className='max-w-screen-2xl mx-auto px-4 py-3 flex justify-between items-center'>
                     <ul className='nav__links'>
                         <li className='link'><Link to="/">Home</Link></li>
                         <li className='link'><Link to="/shop">Shop</Link></li>
